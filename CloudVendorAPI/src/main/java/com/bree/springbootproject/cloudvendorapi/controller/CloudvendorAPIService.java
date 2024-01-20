41e7e3bf9106fe40e133972b456b7bddfa3753a6
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-01-20 11:08:30 +0300

    Updated cloud vendor API

--- CloudVendorAPI/src/main/java/com/bree/springbootproject/cloudvendorapi/controller/CloudvendorAPIService.java
+++ CloudVendorAPI/src/main/java/com/bree/springbootproject/cloudvendorapi/controller/CloudvendorAPIService.java
@@ -1,18 +1,37 @@
 package com.bree.springbootproject.cloudvendorapi.controller;
 
 import com.bree.springbootproject.cloudvendorapi.model.CloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudvendorAPIService {
 
+    CloudVendor cloudVendor;
+
     @GetMapping("{vendorId}")
-    public CloudVendor getCloudVendorDetails(String vendorId,String vendorName, String vendorAddress, String vendorPhoneNummber){
-        return new CloudVendor("C0011","Stephen Hope" ,"234 Kiambu" , "011234589");
+    public CloudVendor getCloudVendorDetails(String vendorId){
+        return cloudVendor;
+
+        /*return new CloudVendor("C011","Stephen Hope", "Kiambu 304","0797710892");*/
     }
 
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor created successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor updated successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(@RequestBody String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor deleted successfully";
+    }
 
 }
