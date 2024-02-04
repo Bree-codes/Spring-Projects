11b5040f3e3551cb4fd53f26a19e73b26a7a8712
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-04 19:24:32 +0300

    added the controller and service classes

---
+++ SendingSMSaandOTP/src/main/java/com/bree/springproject/sendingsmsaandotp/controller/TwilioController.java
@@ -0,0 +1,31 @@
+package com.bree.springproject.sendingsmsaandotp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/otp")
+@Slf4j
+public class TwilioController {
+    @Autowired
+    private SmsService smsService;
+
+    @GetMapping("/process")
+    public String processSMS() {
+        return "SMS sent";
+    }
+
+    @PostMapping("/send-otp")
+    public OtpResponseDto sendOtp(@RequestBody OtpRequest otpRequest) {
+        log.info("inside sendOtp :: "+otpRequest.getUsername());
+        return smsService.sendSMS(otpRequest);
+    }
+    @PostMapping("/validate-otp")
+    public String validateOtp(@RequestBody OtpValidationRequest otpValidationRequest) {
+        log.info("inside validateOtp :: "+otpValidationRequest.getUsername()+" "+otpValidationRequest.getOtpNumber());
+        return smsService.validateOtp(otpValidationRequest);
+    }
+
+}
---
+++ SendingSMSaandOTP/src/main/java/com/bree/springproject/sendingsmsaandotp/service/SmsService.java
@@ -0,0 +1,7 @@
+package com.bree.springproject.sendingsmsaandotp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsService {
+}
