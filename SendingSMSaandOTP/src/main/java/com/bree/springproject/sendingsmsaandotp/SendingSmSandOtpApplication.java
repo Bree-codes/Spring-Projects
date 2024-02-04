e01a112c3f40d1171cc4d0194ca6abac95de822e
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-04 19:19:47 +0300

    Autowired the config class to the main class

--- SendingSMSaandOTP/src/main/java/com/bree/springproject/sendingsmsaandotp/SendingSmSandOtpApplication.java
+++ SendingSMSaandOTP/src/main/java/com/bree/springproject/sendingsmsaandotp/SendingSmSandOtpApplication.java
@@ -1,10 +1,24 @@
 package com.bree.springproject.sendingsmsaandotp;
 
+import com.bree.springproject.sendingsmsaandotp.configuration.TwilioConfig;
+import com.twilio.Twilio;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SendingSmSandOtpApplication {
+
+    @Autowired
+    private TwilioConfig twilioConfig;
+
+    @PostConstruct
+    public void setup() {
+        Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SendingSmSandOtpApplication.class, args);
