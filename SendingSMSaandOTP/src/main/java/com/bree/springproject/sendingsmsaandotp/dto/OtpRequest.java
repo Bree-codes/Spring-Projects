6393d325d5bc864fda0610c2ec283c87413ef32c
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-04 19:29:47 +0300

    added the dto class

---
+++ SendingSMSaandOTP/src/main/java/com/bree/springproject/sendingsmsaandotp/dto/OtpRequest.java
@@ -0,0 +1,14 @@
+package com.bree.springproject.sendingsmsaandotp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OtpRequest {
+    private String username;
+    private String phoneNumber;
+
+}
--- SendingSMSaandOTP/src/main/java/com/bree/springproject/sendingsmsaandotp/service/SmsService.java
+++ SendingSMSaandOTP/src/main/java/com/bree/springproject/sendingsmsaandotp/service/SmsService.java
@@ -1,7 +1,70 @@
 package com.bree.springproject.sendingsmsaandotp.service;
 
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bree.springproject.sendingsmsaandotp.configuration.TwilioConfig;
+import com.bree.springproject.sendingsmsaandotp.dto.OtpRequest;
+import com.bree.springproject.sendingsmsaandotp.dto.OtpResponseDto;
+import com.bree.springproject.sendingsmsaandotp.dto.OtpStatus;
+import com.bree.springproject.sendingsmsaandotp.dto.OtpValidationRequest;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class SmsService {
+
+    @Autowired
+    private TwilioConfig twilioConfig;
+    Map<String, String> otpMap = new HashMap<>();
+
+
+    public OtpResponseDto sendSMS(OtpRequest otpRequest) {
+        OtpResponseDto otpResponseDto = null;
+        try {
+            PhoneNumber to = new PhoneNumber(otpRequest.getPhoneNumber());//to
+            PhoneNumber from = new PhoneNumber(twilioConfig.getPhoneNumber()); // from
+            String otp = generateOTP();
+            String otpMessage = "Dear Customer , Your OTP is  " + otp + " for sending sms through Spring boot application. Thank You.";
+            Message message = Message
+                    .creator(to, from,
+                            otpMessage)
+                    .create();
+            otpMap.put(otpRequest.getUsername(), otp);
+            otpResponseDto = new OtpResponseDto(OtpStatus.DELIVERED, otpMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            otpResponseDto = new OtpResponseDto(OtpStatus.FAILED, e.getMessage());
+        }
+        return otpResponseDto;
+    }
+
+    public String validateOtp(OtpValidationRequest otpValidationRequest) {
+        Set<String> keys = otpMap.keySet();
+        String username = null;
+        for(String key : keys)
+            username = key;
+        if (otpValidationRequest.getUsername().equals(username)) {
+            otpMap.remove(username,otpValidationRequest.getOtpNumber());
+            return "OTP is valid!";
+        } else {
+            return "OTP is invalid!";
+        }
+    }
+
+    private String generateOTP() {
+        return new DecimalFormat("000000")
+                .format(new Random().nextInt(999999));
+    }
+
 }
