432f32482d7299fc06222794bd1ff469491d6ae2
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-05 14:18:04 +0300

    created the OtpRequest class

---
+++ BursaryAppOTPs/src/test/java/com/bree/springproject/bursaryappotps/BursaryAppOtPsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bree.springproject.bursaryappotps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BursaryAppOtPsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
