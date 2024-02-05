94b273b3e229c69d828a3f94b4b124e1a750da1b
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-05 14:31:12 +0300

    created application properties

--- BursaryAppOTPs/src/main/java/com/bree/springproject/bursaryappotps/configuration/TwilioConfig.java
+++ BursaryAppOTPs/src/main/java/com/bree/springproject/bursaryappotps/configuration/TwilioConfig.java
@@ -1,4 +1,18 @@
 package com.bree.springproject.bursaryappotps.configuration;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Component
+@ConfigurationProperties(prefix = "twilio")
+@Data
 public class TwilioConfig {
+
+    private String accountSid;
+    private String phoneNumber;
+    private String authToken;
 }
