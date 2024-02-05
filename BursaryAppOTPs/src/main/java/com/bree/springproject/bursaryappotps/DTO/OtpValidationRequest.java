48b81cf40c94f87d635bbcc2958d2d3455bdc624
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-05 14:26:34 +0300

    created Twilio configuration class

--- BursaryAppOTPs/src/main/java/com/bree/springproject/bursaryappotps/DTO/OtpValidationRequest.java
+++ BursaryAppOTPs/src/main/java/com/bree/springproject/bursaryappotps/DTO/OtpValidationRequest.java
@@ -1,4 +1,13 @@
 package com.bree.springproject.bursaryappotps.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OtpValidationRequest {
+    private String username;
+    private String otpNumber;
 }
---
+++ BursaryAppOTPs/src/main/java/com/bree/springproject/bursaryappotps/configuration/TwilioConfig.java
@@ -0,0 +1,4 @@
+package com.bree.springproject.bursaryappotps.configuration;
+
+public class TwilioConfig {
+}
