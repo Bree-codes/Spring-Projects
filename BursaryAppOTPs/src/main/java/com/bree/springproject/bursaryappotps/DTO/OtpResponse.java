8279a6ae85c62b6f27f23bfa9854b698d3da12b8
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-05 14:21:32 +0300

    created the OtpStatus class

--- BursaryAppOTPs/src/main/java/com/bree/springproject/bursaryappotps/DTO/OtpResponse.java
+++ BursaryAppOTPs/src/main/java/com/bree/springproject/bursaryappotps/DTO/OtpResponse.java
@@ -1,4 +1,13 @@
 package com.bree.springproject.bursaryappotps.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OtpResponse {
+    private OtpStatus status;
+    private String message;
 }
